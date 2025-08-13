b04892a2f664594e3f8a527952fec852e1b563a5
xesam <xesam@outlook.com>
2025-08-13 10:30:38 +0800

    tstack /update

--- vite-tanstack/vite.config.ts
+++ vite-tanstack/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import pkg from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react(), tailwindcss(), tsconfigPaths()],
+    plugins: [tanstackRouter({ autoCodeSplitting: true }), react(), tailwindcss(), tsconfigPaths()],
     define: {
         'import.meta.env.version': JSON.stringify(pkg.version)
     }
